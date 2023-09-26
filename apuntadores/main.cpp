bb5786cf5d472caadace4c49c886aa2524e9467a
Frank0125 <A01252974@tec.mx>
2023-09-25 19:11:57 -0700

    Eso tilin

---
+++ apuntadores/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "stack.cpp"
+
+
+int main(){
+    Stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.isEmpty() << endl;
+    return 0;
+}
