35c292664f0fffdbfb5157f6aaf982bce52be21e
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2023-10-15 22:48:39 -0700

    added all testcases

--- main.cpp
+++ main.cpp
@@ -6,17 +6,27 @@
 
 int main() {
     try {
+        // Heap Tree Basic Structure - For the testcases
+
         priorityQueue pq(10);
-        pq.push(5);
-        pq.push(10);
+
+        // Push Testcases
+
+        pq.push(5);     // First Case
+        pq.push(10);    // Second Case
         pq.push(15);
         pq.push(20);
-        pq.push(4);
+        pq.push(4);     // Third Case
+        pq.push(25);    // Fourth Case
 
-        for (int i = 0; i < 5; ++i) {
-            std::cout << pq.top() << std::endl;
+        // Pop, Top, Empty and Size Testcases
+
+        while (!pq.empty()) {                            // A case for each loop (six in total)
+            std::cout << "Top: "<< pq.top() << std::endl;
+            std::cout << "Size: "<< pq.size() << std::endl;
             pq.pop();
         }
+
     } catch (const std::runtime_error &e) {
         std::cerr << e.what() << std::endl;
     }
